9048b4325b1c65e78c4419bef679ccd4c8cf6149
pym-elonsoft <uandme1010@elonsoft.co.kr>
2023-10-02 15:18:18 +0900

    Initial commit

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    //conuurrentHashMap 동시성 이슈로 인해 사실 이걸 써야한다고 함
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+
+}
