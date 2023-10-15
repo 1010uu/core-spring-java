659dbea02964508144ebec9e1f2695b9722bcc34
pym-elonsoft <uandme1010@elonsoft.co.kr>
2023-10-15 14:37:31 +0900

    xml설정을 통한 빈 조회

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,20 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        //resource 밑에 있는 설정정보를 읽어옴
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
